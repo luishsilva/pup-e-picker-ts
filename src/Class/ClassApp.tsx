0cc150c1090c7dc814da95a6080094c4d4b536a5
luishsilva <luizhrick@gmail.com>
2024-05-07 09:42:49 -0600

    Set active tab setActive state on ClassSection and ClassApp

--- src/Class/ClassApp.tsx
+++ src/Class/ClassApp.tsx
@@ -3,16 +3,33 @@ import { ClassSection } from "./ClassSection";
 import { ClassDogs } from "./ClassDogs";
 import { ClassCreateDogForm } from "./ClassCreateDogForm";
 
-export class ClassApp extends Component {
+type State = {
+  activeTab: number
+}
+
+export class ClassApp extends Component<State> {
+
+  state: State = {
+    activeTab: 0
+  }
+
+ setActiveTab = (tabIndex: number) => this.setState({activeTab: tabIndex})
+
   render() {
+
+    const { activeTab } = this.state
+
     return (
       <div className="App" style={{ backgroundColor: "goldenrod" }}>
         <header>
           <h1>pup-e-picker (Class Version)</h1>
         </header>
-        <ClassSection>
-          <ClassDogs />
-          <ClassCreateDogForm />
+        <ClassSection
+          activeTab={activeTab}
+          setActiveTab={this.setActiveTab}
+        >
+          {activeTab <= 2 && <ClassDogs />}
+          {activeTab === 3 && <ClassCreateDogForm />}
         </ClassSection>
       </div>
     );
--- src/Class/ClassSection.tsx
+++ src/Class/ClassSection.tsx
@@ -3,12 +3,16 @@ import { Component, ReactNode } from "react";
 import { Link } from "react-router-dom";
 
 type ClassSectionProps = {
-  children: ReactNode
+  activeTab: number,
+  children: ReactNode,
+  setActiveTab: (tabIndex: number) => void
 }
 
 export class ClassSection extends Component<ClassSectionProps> {
   render() {
-  
+
+    const { activeTab, setActiveTab } = this.props;
+
     return (
       <section id="main-section">
         <div className="container-header">
@@ -19,16 +23,21 @@ export class ClassSection extends Component<ClassSectionProps> {
           </Link>
 
           <div className="selectors">
+            {/* This should display the all Dogs */}
+            <div className={`selector ${activeTab === 0 && 'active'}`} onClick={() => setActiveTab(0)} >
+              All Dogs ( 0 )
+            </div>
+
             {/* This should display the favorited count */}
-            <div className={`selector`} onClick={() => {}}>
+            <div className={`selector ${activeTab === 1 && 'active'}`} onClick={() => setActiveTab(1)} >
               favorited ( 0 )
             </div>
 
             {/* This should display the unfavorited count */}
-            <div className={`selector`} onClick={() => {}}>
+            <div className={`selector ${activeTab === 2 && 'active'}`} onClick={() => setActiveTab(2)} >
               unfavorited ( 0 )
             </div>
-            <div className={`selector active`} onClick={() => {}}>
+            <div className={`selector ${activeTab === 3 && 'active'}`} onClick={() => setActiveTab(3)} >
               create dog
             </div>
           </div>
